9273abf9467fa96bb606de10ab885965d5637179
Adam Meszaros <xmeszar2@fi.muni.cz>
2024-11-30 23:24:24 +0100

    Feat: Add basic login page

--- lib/page/login/login_page.dart
+++ lib/page/login/login_page.dart
@@ -1,33 +1,68 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:stable/page/household/edit_household_page.dart';
-import 'package:stable/page/task/task_view.dart';
+import 'package:get_it/get_it.dart';
+import 'package:stable/authentication/auth_service.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final AuthService _auth = GetIt.instance<AuthService>();
+
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(children: [
-        ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => EditHouseholdPage()),
-            );
-          },
-          child: const Text('Create household'),
+      appBar: AppBar(title: Text('Login')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                User? user = await _auth.signInWithEmailAndPassword(
+                  _emailController.text,
+                  _passwordController.text,
+                );
+                if (user != null) {
+                  Navigator.pushReplacementNamed(context, '/home');
+                }
+              },
+              child: Text('Login'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                User? user = await _auth.registerWithEmailAndPassword(
+                  _emailController.text,
+                  _passwordController.text,
+                );
+                if (user != null) {
+                  Navigator.pushReplacementNamed(context, '/home');
+                }
+              },
+              child: Text('Register'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                User? user = await _auth.signInWithGoogle();
+                if (user != null) {
+                  Navigator.pushReplacementNamed(context, '/home');
+                }
+              },
+              child: Text('Sign in with Google'),
+            ),
+          ],
         ),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => TaskView()),
-            );
-          },
-          child: const Text('View tasks'),
-        ),
-      ]),
+      ),
     );
   }
 }
