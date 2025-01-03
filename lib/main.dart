236c7ac28016d599169fb8e0fe91540a0ca60214
Adam Meszaros <xmeszar2@fi.muni.cz>
2025-01-03 22:25:38 +0100

    Feat: Implement Google OAuth

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
-import 'package:google_sign_in_web/google_sign_in_web.dart';
 import 'package:stable/app/widget/app_wrapper.dart';
-import 'package:stable/authentication/auth_service.dart';
 import 'package:stable/database/service/database_service.dart';
 import 'package:stable/service/task_service.dart';
 import 'package:stable/firebase_options.dart';
 import 'package:stable/model/task/task.dart';
 
-import 'model/subtask/subtask.dart';
+import 'package:stable/model/subtask/subtask.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,9 +19,15 @@ Future<void> main() async {
   await dotenv.load(fileName: ".env");
 
   // Initialize Firebase platform
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Initialize Firebase Auth providers
+  // For Google sign-in, you need to add the client ID to .env file
+  // Client ID can be found in the Google Cloud Console
+  FirebaseUIAuth.configureProviders([
+    GoogleProvider(clientId: dotenv.env['GOOGLE_CLIENT_ID']!),
+  ]);
 
   // Register Firestore documents
   GetIt.instance.registerSingleton(
@@ -35,9 +41,6 @@ Future<void> main() async {
   GetIt.instance.registerSingleton(TaskService(
       GetIt.instance<DatabaseService<Task>>(),
       GetIt.instance<DatabaseService<Subtask>>()));
-
-  // Start Firebase Authentication service
-  GetIt.instance.registerSingleton(AuthService());
 
   runApp(const AppWrapper());
 }
--- lib/page/home/home_page.dart
+++ lib/page/home/home_page.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:stable/common/widget/page_template.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return PageTemplate(
+      title: "Household Name Placeholder",
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.pushReplacementNamed(context, '/tasks');
+        },
+        child: Text("Tasks"),
+      ),
+    );
     // TODO add welcome message
     // TODO add an overview of the household (unfinished repeating tasks, rotary tasks)
     // TODO add a center floating button to quickly add a new task
