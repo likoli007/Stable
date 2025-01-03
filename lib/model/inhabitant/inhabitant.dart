232eca9fc1269dc14b42b9420de7b9b4b71bea76
Adam Meszaros <xmeszar2@fi.muni.cz>
2025-01-03 22:25:59 +0100

    Feat: Add missing imports

--- lib/model/inhabitant/inhabitant.dart
+++ lib/model/inhabitant/inhabitant.dart
@@ -1,7 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:json_annotation/json_annotation.dart';
-import 'package:stable/database/util/document_reference_converter.dart';
-
 part 'inhabitant.g.dart';
 
 @JsonSerializable(explicitToJson: true, includeIfNull: true)
