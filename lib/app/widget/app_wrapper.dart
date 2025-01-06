49ad7c3aaaaee1bda6d53b44e7f0eda766ccea9c
Adam Meszaros <xmeszar2@fi.muni.cz>
2025-01-06 19:45:43 +0100

    Refactor: Implement loadingStreamBulider

--- lib/app/widget/app_wrapper.dart
+++ lib/app/widget/app_wrapper.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:stable/auth/firebase_auth_service.dart';
 import 'package:stable/common/theme/theme_provider.dart';
+import 'package:stable/model/settings/settings.dart';
 import 'package:stable/page/home/home_page.dart';
-import 'package:stable/page/household/household_page.dart';
 import 'package:stable/page/login/introduction_page.dart';
 import 'package:stable/page/task/task_page.dart';
 import 'package:stable/service/settings_controller.dart';
+import 'package:stable/common/widget/loading_stream_builder.dart';
 
 class AppWrapper extends StatelessWidget {
   AppWrapper({super.key});
@@ -17,40 +18,32 @@ class AppWrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-        stream: _settingsController.settingsStream,
-        builder: (context, settingsSnapshot) {
-          if (settingsSnapshot.hasError) {
-            return Text('Error: ${settingsSnapshot.error}');
-          }
+    return LoadingStreamBuilder<Settings>(
+      stream: _settingsController.settingsStream,
+      builder: _materialAppBuilder,
+    );
+  }
 
-          if (!settingsSnapshot.hasData) {
-            return CircularProgressIndicator();
-          }
-
-          final settings = settingsSnapshot.data!;
-
-          return MaterialApp(
-              title: 'Stable',
-              debugShowCheckedModeBanner: false,
-              themeMode: settings.themeMode,
-              theme: _themeProvider.themeSeeded(
-                seedColor: settings.themeColor,
-              ),
-              darkTheme: _themeProvider.themeSeeded(
-                seedColor: settings.themeColor,
-                brightness: Brightness.dark,
-              ),
-              initialRoute: _auth.user == null ? '/' : '/home',
-              routes: {
-                '/': (context) => IntroductionPage(),
-                '/home': (context) => HomePage(),
-                '/tasks': (context) => TaskPage(),
-                //TODO '/household': (context) => HouseholdPage(),
-                //TODO: Add routes
-              });
-          //TODO: Add responsive design for landscape desktop users
-          //TODO: Add theme switcher (and color buttons)
-        });
+  Widget _materialAppBuilder(BuildContext context, Settings settings) {
+    return MaterialApp(
+      title: 'Stable',
+      debugShowCheckedModeBanner: false,
+      themeMode: settings.themeMode,
+      theme: _themeProvider.themeSeeded(
+        seedColor: settings.themeColor,
+      ),
+      darkTheme: _themeProvider.themeSeeded(
+        seedColor: settings.themeColor,
+        brightness: Brightness.dark,
+      ),
+      initialRoute: _auth.user == null ? '/' : '/home',
+      routes: {
+        '/': (context) => IntroductionPage(),
+        '/home': (context) => HomePage(),
+        '/tasks': (context) => TaskPage(),
+        //TODO '/household': (context) => HouseholdPage(),
+        //TODO: Add routes
+      },
+    );
   }
 }
