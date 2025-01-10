69a20123f353fcbf053b688d079b341867cf471b
Adam Meszaros <xmeszar2@fi.muni.cz>
2025-01-10 14:28:53 +0100

    Feat: Add icon to introductionPage

--- lib/page/login/introduction_page.dart
+++ lib/page/login/introduction_page.dart
@@ -9,45 +9,48 @@ class IntroductionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(children: [
-      // TODO Add introduction to the app
-      Spacer(),
-      //TODO Add image of a stable
-      Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Column(
-            children: [
-              SizedBox(height: STANDARD_GAP),
-              Text(
-                "Household tasks? No problem.",
-                textScaler: TextScaler.linear(HEADLINE_SCALER),
-              ),
-              Text(
-                "Dirty dishes, laundry, or piled-up trash? "
-                "Create tasks, assign them to your household members, and keep track of their progress. "
-                "Just sit back and relax — Stable will take care of the rest.",
-                textScaler: TextScaler.linear(INFO_PARAGRAPH_SCALER),
-              ),
-            ],
-          ),
-          SizedBox(height: STANDARD_GAP),
-          SizedBox(
-            width: double.infinity,
-            child: FullWidthButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => RegisterScreen()),
-                );
-              },
-              icon: Icon(Icons.bedroom_baby),
-              label: "Enter the Stable",
-              alignment: Alignment.center,
+        body: Padding(
+      padding: const EdgeInsets.all(STANDARD_GAP),
+      child: Column(children: [
+        Spacer(),
+        Icon(Icons.bedroom_baby, size: 300),
+        SizedBox(height: 3 * STANDARD_GAP),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Column(
+              children: [
+                SizedBox(height: STANDARD_GAP),
+                Text(
+                  "Household tasks? No problem.",
+                  textScaler: TextScaler.linear(HEADLINE_SCALER),
+                ),
+                Text(
+                  "Dirty dishes, laundry, or piled-up trash? "
+                  "Create tasks, assign them to your household members, and keep track of their progress. "
+                  "Just sit back and relax — Stable will take care of the rest.",
+                  textScaler: TextScaler.linear(INFO_PARAGRAPH_SCALER),
+                ),
+              ],
             ),
-          ),
-        ],
-      ),
-    ]));
+            SizedBox(height: STANDARD_GAP),
+            SizedBox(
+              width: double.infinity,
+              child: FullWidthButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => RegisterScreen()),
+                  );
+                },
+                icon: Icon(Icons.bedroom_baby),
+                label: "Enter the Stable",
+                alignment: Alignment.center,
+              ),
+            ),
+          ],
+        ),
+      ]),
+    ));
   }
 }
