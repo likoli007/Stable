82f02ef771e4a7f48f229293d8d1ea76e3bd612e
Adam Meszaros <xmeszar2@fi.muni.cz>
2025-01-15 10:40:37 +0100

    Refactor: Delete unnecessary files

--- lib/common/theme/color_extension.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const _LUMINANCE_THRESHOLD = 0.5;
-
-extension ColorExtension on Color {
-  Color getForegroundColorDependingOnBackground({double luminanceThreshold = _LUMINANCE_THRESHOLD}) =>
-      computeLuminance() > luminanceThreshold ? Colors.black87 : Colors.white;
-}
