ef1b5a4678628b81f0d1cccb0e9371029d9c1ff7
Adam Meszaros <xmeszar2@fi.muni.cz>
2025-01-16 00:28:45 +0100

    Feat: Manage Household

--- lib/ui/page/household/share_household_page.dart
+++ lib/ui/page/household/share_household_page.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:stable/service/household_service.dart';
+import 'package:stable/ui/common/page/page_body.dart';
 
 class ShareHouseholdPage extends StatelessWidget {
-  final String householdReference;
+  final String groupId;
 
-  const ShareHouseholdPage({super.key, required this.householdReference});
+  const ShareHouseholdPage({super.key, required this.groupId});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
-    // TODO share code and information
-    // TODO make a dialog out of this
+    return PageBody(
+      title: 'Share Household',
+      body: Column(
+        children: [
+          const Text('Share this household with others'),
+          Text("GroupId: $groupId")
+        ],
+      ),
+    );
+    // TODO share android popup
+    // TODO make a big icon page out of this
   }
 }
